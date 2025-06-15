7208bae298bf20a9eb82092c7173b25ee1432f97
Radhika Agarwal <124703676+radhikaaga@users.noreply.github.com>
2025-06-15 21:32:03 +0530

    Add files via upload

---
+++ Prob8/Regex_Validation.py
@@ -0,0 +1,9 @@
+# Number of test cases
+T = int(input())
+
+for _ in range(T):
+    a, b = input().split()
+    try:
+        print(int(a) // int(b))
+    except (ZeroDivisionError, ValueError) as e:
+        print("Error Code:", e)
